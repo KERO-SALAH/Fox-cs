4b296c4d2e061a1575f01b9f476c2c5354e9a4f1
Kerls2020 <Salah.saidmansy@gmail.com>
2024-10-02 11:34:00 +0300

    up date parts and moved it

--- js/main.js
+++ js/main.js
@@ -14,20 +14,20 @@ document.querySelectorAll(".smooth-scroll").forEach((anchor) => {
   });
 });
 // الهيدر
-fetch("https://kero-salah.github.io/fox---Copy/header.html")
+fetch("https://kero-salah.github.io/Fox-cs/parts/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
   });
 //الفوتر
-fetch("../footer.html")
+fetch("https://kero-salah.github.io/Fox-cs/parts/footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
   });
 
 //القائمة الجانبية
-fetch("../aside.html")
+fetch("https://kero-salah.github.io/Fox-cs/parts/aside.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("asider").innerHTML = data;
@@ -42,7 +42,7 @@ function openSidebar() {
   }
 }
 //عرض الازرار الجانبية مثل الهوم و الاب
-fetch("../go_home.html")
+fetch("https://kero-salah.github.io/Fox-cs/parts/go_home.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("go_home").innerHTML = data;
